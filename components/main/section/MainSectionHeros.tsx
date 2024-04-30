42facd80186d2f10d89b7114a76f9ba58fa0f920
yusufdgzl <yusuf.dg13@gmail.com>
2024-04-30 14:17:46 +0300

    MainSectionHeros styles comleated

--- components/main/section/MainSectionHeros.tsx
+++ components/main/section/MainSectionHeros.tsx
@@ -30,15 +30,37 @@ export default function MainSectionHeros() {
       <div className="flex relative bg-slate-200 py-24  rounded-xl items-center justify-between pl-10 overflow-hidden ">
         <div className="min-w-[160px] space-y-6 md:pl-20">
           <h2 className="text-2xl font-semibold tracking-wider">ZX7 SPEAKER</h2>
-          <button className="border border-black px-8 py-3 text-sm font-semibold hover:text-white hover:bg-black">SEE PRODUCT</button>
+          <button className="border border-black px-8 py-3 text-sm font-semibold hover:text-white hover:bg-black">
+            SEE PRODUCT
+          </button>
         </div>
         <div className="absolute -right-28 md:-right-4 md:-top-40 lg:-top-[460px]">
-          <Image className=" w-[250px] md:w-[400px] lg:w-[700px]" src="/images/hero-speaker-zx7.png" width={1000} height={1000} alt="zx7 speaker"/>
+          <Image
+            className=" w-[250px] md:w-[400px] lg:w-[700px]"
+            src="/images/hero-speaker-zx7.png"
+            width={1000}
+            height={1000}
+            alt="zx7 speaker"
+          />
         </div>
       </div>
 
+      <div className="grid space-y-6 md:grid-cols-2 md:space-y-0 md:space-x-6">
+        <div className="flex flex-col h-44 items-center justify-center  rounded-xl overflow-hidden md:h-64">
+          <Image
+            className="rounded-xl w-full "
+            src="/images/earphones-hero.jpg"
+            width={400}
+            height={400}
+            alt="earphones"
+          />
+        </div>
 
-      <div></div>
+        <div className=" justify-between items-start flex flex-col h-44 md:h-full bg-gray-200 rounded-xl  p-6 py-8 md:px-24 md:py-16   ">
+          <h2 className="text-2xl font-semibold tracking-wider md:text-3xl">YX1 EARPHONES</h2>
+          <button className="border border-black px-6 py-3 text-sm font-semibold hover:bg-black hover:text-white md:px-8">SEE PRODUCT</button>
+        </div>
+      </div>
     </div>
   );
 }

25d57a2b5d9e877fd8b911f548002994cfb61a56
yusufdgzl <yusuf.dg13@gmail.com>
2024-05-01 18:59:19 +0300

    product.json created

--- app/category/headphones/page.tsx
+++ app/category/headphones/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import CategoriesSection from "@/components/categories/CategoriesSection";
 import AudioGearHero from "@/components/main/section/AudioGearHero";
 
+import headphones from "@/data/products.json";
+import Image from "next/image";
+
 export default function HeadPhones() {
+  const headphonesData = headphones;
+
   return (
     <>
       <div className="flex flex-col ">
@@ -10,7 +17,30 @@ export default function HeadPhones() {
         </h1>
       </div>
       <div className="max-w-[1400px] mx-auto">
-        <div></div>
+
+        <div>
+          {headphonesData.map((item) => (
+             <div className=" grid gap-4 py-20 md:px-20 md:py-40 md:grid-cols-2">
+             <Image
+               className=" bg-black rounded-xl md:h-[600px] md:w-[600px]  "
+               src={item.images[1]}
+               width={700}
+               height={600}
+               alt="audio gear"
+             />
+             <div className="flex flex-col items-center justify-center text-center space-y-6 mx-auto py-4 max-w-[500px] md:items-start md:text-left">
+               <h2 className="text-3xl font-semibold md:text-4xl">
+                 {item.title}
+               </h2>
+               <p className="text-md md:text-xl opacity-60">
+                 {item.description}
+               </p>
+             </div>
+           </div>
+          ))}
+        </div>
+
+
         <div className="px-6 pt-40">
           <CategoriesSection />
           <AudioGearHero />

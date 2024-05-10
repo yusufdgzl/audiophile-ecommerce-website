d5f6a2812b452110cfe02bafef625513bb75f52d
yusufdgzl <yusuf.dg13@gmail.com>
2024-05-10 22:13:11 +0300

    CartShopping styles edited

--- components/cart/CartShopping.tsx
+++ components/cart/CartShopping.tsx
@@ -7,6 +7,7 @@ import CartShoppingProductsItem from "./CartShoppingProductsItem";
 export default function CartShopping() {
   const cartTotal = useAppSelector((state) => state.cart.total);
   const cartItems = useAppSelector((state) => state.cart.items);
+  const cartTotalAmount = useAppSelector((state)=> state.cart.totalAmount)
 
   
 
@@ -14,15 +15,18 @@ export default function CartShopping() {
 
   return (
     <div className="bg-white space-y-6 p-6  lg:w-[350px] rounded-xl ">
-      <h2 className="text-lg font-semibold">SUMMARY</h2>
-
+        <div className="flex justify-between">
+        <h2 className="text-lg font-semibold tracking-widest">CART({cartTotalAmount})</h2>
+        <button className=" underline opacity-40 hover:text-orange-700">Remove all</button>
+        </div>
+      
       {cartItems.map((item) => (
         <CartShoppingProductsItem key={item.id} {...item} />
       ))}
 
       <div className="space-y-2">
-        <div className="flex justify-between">
-          <p>TOTAL</p>
+        <div className="flex justify-between items-center">
+          <p className="opacity-45">TOTAL</p>
           <p className="font-semibold text-lg tracking-wider">${cartTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
         </div>
       </div>
--- components/cart/CartShoppingProductsItem.tsx
+++ components/cart/CartShoppingProductsItem.tsx
@@ -2,26 +2,39 @@ import { ProductsType } from "@/app/category/headphones/page";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function CartShoppingProductsItem(props:ProductsType){
+export default function CartShoppingProductsItem(props: ProductsType) {
+  const { images, title, price, amount, id } = props;
 
-    const {images,title,price,amount,id} = props;
-
-    return(
-        <div className="flex justify-between items-center">
-        <Link href={`/product/${id}`} className="flex group items-center space-x-4">
-          <Image
-            src={images.main}
-            className=" rounded-xl group-hover:scale-105 duration-300"
-            alt='products'
-            width={64}
-            height={64}
-          />
-          <div>
-            <p className="group-hover:text-orange-500">{title.substring(0,4)}</p>
-            <p className=" opacity-60">${price}</p>
-          </div>
-        </Link>
-        <p className=" opacity-70">x{amount}</p>
+  return (
+    <div className="flex justify-between items-center">
+      <Link
+        href={`/product/${id}`}
+        className="flex group items-center space-x-4"
+      >
+        <Image
+          src={images.main}
+          className=" rounded-xl group-hover:scale-105 duration-300"
+          alt="products"
+          width={64}
+          height={64}
+        />
+        <div>
+          <p className="group-hover:text-orange-500">{title.substring(0, 4)}</p>
+          <p className=" opacity-60">${price}</p>
+        </div>
+      </Link>
+      <div className="flex flex-col items-end ">
+        <div className="bg-gray-200 py-2 px-2 flex justify-around rounded-sm">
+          <button className="px-3 hover:text-orange-500">-</button>
+          <label htmlFor="amount">
+            <input className=" outline-none bg-gray-200 w-6 text-center " type="text" id="amount" maxLength={2} defaultValue={1} />
+          </label>
+          <button className="px-3 hover:text-orange-500">+</button>
+        </div>
+        <button className="underline opacity-40 hover:text-orange-700">
+          Remove
+        </button>
       </div>
-    )
+    </div>
+  );
 }

b72f8191c809af07a45b5d9c9487118942efed2b
yusufdgzl <yusuf.dg13@gmail.com>
2024-05-13 11:46:26 +0300

    Empty state added to CartShopping

--- components/cart/CartShopping.tsx
+++ components/cart/CartShopping.tsx
@@ -8,32 +8,52 @@ import { cartSlice } from "@/lib/features/cart/cartSlice";
 export default function CartShopping() {
   const cartTotal = useAppSelector((state) => state.cart.total);
   const cartItems = useAppSelector((state) => state.cart.items);
-  const cartTotalAmount = useAppSelector((state)=> state.cart.totalAmount)
+  const cartTotalAmount = useAppSelector((state) => state.cart.totalAmount);
 
   const dispatch = useAppDispatch();
 
-  function clearCartHandler(){
-    dispatch(cartSlice.actions.clearAllCart())
+  function clearCartHandler() {
+    dispatch(cartSlice.actions.clearAllCart());
   }
 
   return (
-    <div className="bg-white space-y-6 p-6  lg:w-[350px] rounded-xl ">
-        <div className="flex justify-between">
-        <h2 className="text-lg font-semibold tracking-widest">CART({cartTotalAmount})</h2>
-        <button onClick={clearCartHandler} className=" underline opacity-40 hover:text-orange-700">Remove all</button>
-        </div>
-      
-      {cartItems.map((item) => (
-        <CartShoppingProductsItem key={item.id} {...item} />
-      ))}
-
-      <div className="space-y-2">
-        <div className="flex justify-between items-center">
-          <p className="opacity-45">TOTAL</p>
-          <p className="font-semibold text-lg tracking-wider">${cartTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-        </div>
-      </div>
-      <Link href="/checkout" className="btn-orange w-full flex justify-center">CHECKOUT</Link>
+    <div className="bg-white space-y-6 p-8  lg:w-[350px] rounded-xl ">
+      {cartItems.length === 0 ? (
+        <p className="font-normal  text-center">Your cart is empty!</p>
+      ) : (
+        <>
+          <div className="flex justify-between">
+            <h2 className="text-lg font-semibold tracking-widest">
+              CART({cartTotalAmount})
+            </h2>
+            <button
+              onClick={clearCartHandler}
+              className=" underline opacity-40 hover:text-orange-700"
+            >
+              Remove all
+            </button>
+          </div>
+          <div>
+            {cartItems.map((item) => (
+              <CartShoppingProductsItem key={item.id} {...item} />
+            ))}
+          </div>
+          <div className="space-y-2">
+            <div className="flex justify-between items-center">
+              <p className="opacity-45">TOTAL</p>
+              <p className="font-semibold text-lg tracking-wider">
+                ${cartTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </p>
+            </div>
+          </div>
+          <Link
+            href="/checkout"
+            className="btn-orange w-full flex justify-center"
+          >
+            CHECKOUT
+          </Link>
+        </>
+      )}
     </div>
   );
 }
